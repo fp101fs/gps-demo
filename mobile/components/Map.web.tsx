331814d1901aaf59a5029ff725e7e00d42dd059b
fp101fs <136630859+fp101fs@users.noreply.github.com>
2026-01-11 21:53:46 +0700

    style: fix marker proportions and slow down pulse animation

--- mobile/components/Map.web.tsx
+++ mobile/components/Map.web.tsx
@@ -42,9 +42,9 @@ export default function Map({ currentPoint, points, isReplayMode }: MapProps) {
     
     const blueIcon = L.icon({
         iconUrl: Asset.fromModule(require('../assets/images/marker-blue.webp')).uri,
-        iconSize: [40, 60],
-        iconAnchor: [20, 60],
-        popupAnchor: [1, -34],
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40],
         className: 'marker-pulse'
     });
     
@@ -62,9 +62,9 @@ export default function Map({ currentPoint, points, isReplayMode }: MapProps) {
     if (!markerRef.current) {
         const blueIcon = L.icon({
             iconUrl: Asset.fromModule(require('../assets/images/marker-blue.webp')).uri,
-            iconSize: [40, 60],
-            iconAnchor: [20, 60],
-            popupAnchor: [1, -34],
+            iconSize: [40, 40],
+            iconAnchor: [20, 40],
+            popupAnchor: [0, -40],
             className: 'marker-pulse'
         });
                 markerRef.current = L.marker([currentPoint.lat, currentPoint.lng], { icon: blueIcon }).addTo(mapRef.current);
