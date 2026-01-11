270229c9ac2cfaf9025d53daadd70990f1b517b4
fp101fs <136630859+fp101fs@users.noreply.github.com>
2026-01-11 21:26:38 +0700

    feat: use marker-blue.webp for map marker

--- mobile/components/Map.web.tsx
+++ mobile/components/Map.web.tsx
@@ -40,7 +40,7 @@ export default function Map({ currentPoint, points, isReplayMode }: MapProps) {
     }).addTo(mapRef.current);
     
     const blueIcon = L.icon({
-        iconUrl: require('../assets/images/marker-blue.svg'),
+        iconUrl: require('../assets/images/marker-blue.webp'),
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
@@ -59,7 +59,7 @@ export default function Map({ currentPoint, points, isReplayMode }: MapProps) {
 
     if (!markerRef.current) {
         const blueIcon = L.icon({
-            iconUrl: require('../assets/images/marker-blue.svg'),
+            iconUrl: require('../assets/images/marker-blue.webp'),
             iconSize: [25, 41],
             iconAnchor: [12, 41],
             popupAnchor: [1, -34],
