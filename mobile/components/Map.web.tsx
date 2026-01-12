ee030f131712c06a1eef3f7f05dec77b8bd7d849
fp101fs <136630859+fp101fs@users.noreply.github.com>
2026-01-12 17:02:08 +0700

    style: increase green cross marker size to 100x100

--- mobile/components/Map.web.tsx
+++ mobile/components/Map.web.tsx
@@ -42,9 +42,9 @@ export default function Map({ currentPoint, points, isReplayMode }: MapProps) {
     
     const blueIcon = L.icon({
         iconUrl: Asset.fromModule(require('../assets/images/marker-green-cross.png')).uri,
-        iconSize: [50, 50],
-        iconAnchor: [25, 25],
-        popupAnchor: [0, -25],
+        iconSize: [100, 100],
+        iconAnchor: [50, 50],
+        popupAnchor: [0, -50],
         className: 'marker-pulse'
     });
     
@@ -62,9 +62,9 @@ export default function Map({ currentPoint, points, isReplayMode }: MapProps) {
     if (!markerRef.current) {
         const blueIcon = L.icon({
             iconUrl: Asset.fromModule(require('../assets/images/marker-green-cross.png')).uri,
-            iconSize: [50, 50],
-            iconAnchor: [25, 25],
-            popupAnchor: [0, -25],
+            iconSize: [100, 100],
+            iconAnchor: [50, 50],
+            popupAnchor: [0, -50],
             className: 'marker-pulse'
         });
         markerRef.current = L.marker([currentPoint.lat, currentPoint.lng], { icon: blueIcon }).addTo(mapRef.current);
